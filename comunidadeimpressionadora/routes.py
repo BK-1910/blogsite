8d907fc96bf88a1862a22211942c4231b4f5fda4
BK-1910 <brunowuldarczki@gmail.com>
2025-10-14 19:39:48 -0300

    DECODE routes.py
    Adicionado decode("utf-8") para realizar a adequação com o postgres

--- comunidadeimpressionadora/routes.py
+++ comunidadeimpressionadora/routes.py
@@ -57,9 +57,9 @@ def login():
             flash(f'Falha no login! E-mail: {form_login.email.data} ou senha incorretos', 'alert-danger')
     if form_criar_conta.validate_on_submit() and 'botao_submit_criar_conta' in request.form:
         #Encriptografar senha antes da criação do usuário utilizando o bcrypt
-        senha_crypt = bcrypt.generate_password_hash(form_criar_conta.senha.data)
+        senha_crypt = bcrypt.generate_password_hash(form_criar_conta.senha.data).decode("utf-8")
         #Criar o usuário
-        usuario = Usuario(username=form_criar_conta.username.data , email=form_criar_conta.email.data, senha=senha_crypt).decode('utf-8')
+        usuario = Usuario(username=form_criar_conta.username.data , email=form_criar_conta.email.data, senha=senha_crypt)
         with app.app_context():
             database.session.add(usuario) #Adicionar a sessão
             database.session.commit() #Commit na sessão
@@ -190,5 +190,4 @@ def excluir_post(post_id):
         return redirect(url_for('home'))
     else:
         #abort é usado para indicar que tal usuário não tem permissão de realizar tal ação
-
         abort(403)
