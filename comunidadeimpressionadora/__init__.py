8def1367304d71b6af18f8c8a343eda52fa0825c
BK-1910 <brunowuldarczki@gmail.com>
2025-10-14 19:30:43 -0300

    Update __init__.py

--- comunidadeimpressionadora/__init__.py
+++ comunidadeimpressionadora/__init__.py
@@ -4,14 +4,16 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
+
 #Padrão para criação de qualquer site
 app = Flask(__name__)
 
 # Criar configuração do app e banco de dados, dentro do arquivo __init__
 app.secret_key = 'aeecb39ba8c1e461f067730319f1cfa8'
 
-if os.environ.get('DATABASE_URL'):
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+if os.getenv("DATABASE_URL"):
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
@@ -26,6 +28,21 @@ login_manager.login_view = 'login'
 login_manager.login_message = 'Realize o login para acessar a página!'
 #Mudando a cor da caixa com parâmetro 'alert-info' do bootstrap
 login_manager.login_message_category = 'alert-info'
+
+#Importar models, pois lá está a configuração do banco de dados
+from comunidadeimpressionadora import models
+#Criar engine para criar banco de dados caso não exista
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+#Verificar se dentro da engine existe a tabela usuários
+inspector = sqlalchemy.inspect(engine)
+#No inspector não inserir nome do banco sempre em minúsculo
+if not inspector.has_table("usuario"):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print("Base de dados criada!")
+else:
+    print("Base de dados já existente!")
 
 # Importar o arquivo de links 'routes' para poder rodar os links - IMPORTAR NO FINAL DO CÓDIGO, POIS O ROUTES PRECISA DO APP PARA FUNCIONAR
 from comunidadeimpressionadora import routes
